5eaed5bcc83f832a1178e0a7f8700781802c1d90
suleymanibishov <suleyman22022@gmail.com>
2023-11-16 12:48:16 +0400

    Task

---
+++ B-E-Task10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace B_E_Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student tural = new Student();
+            Console.WriteLine(tural.Age); //1
+
+            Student suleyman = new Student();
+            Console.WriteLine(suleyman.Age); //2
+
+            Student gafar = new Student();
+            Console.WriteLine(gafar.Age); //3
+
+            Student semed = new Student();
+            Console.WriteLine(semed.Age); //4
+        }
+    }
+}
---
+++ B-E-Task10/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace B_E_Task10
+{
+    internal class Student
+    {
+        private static int lastAge;
+        public Student()
+        {
+            Age = ++lastAge;
+            
+        }
+        public int Age { get; set; }
+    }
+}
